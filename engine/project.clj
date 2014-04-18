c7298103148b8bd65890a5bb2f6a1ad7e93d3704
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-04-18 11:00:19 -0400

    scoring

--- engine/project.clj
+++ engine/project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.datomic/datomic-free "0.9.4497"]
+                 [com.flyingmachine/datomic-junk "0.1.3"]
+                 [twitter-api "0.7.5"]]
   :main ^:skip-aot magepunch.engine
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
